836608a362daaa0605576941891e3a8a91ea0f34
Yeondam Park <giyen9088@gmail.com>
2019-01-12 11:10:12 -0800

    add AppLayout

---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WelcomeMessage() {
+  return <h1>Welcome to CryptoDash</h1>;
+}
---
+++ src/App/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import WelcomeMessage from "./WelcomeMessage";
+import AppLayout from "./AppLayout";
+
+class App extends Component {
+  render() {
+    return (
+      <AppLayout>
+        <WelcomeMessage />
+      </AppLayout>
+    );
+  }
+}
+
+export default App;
