d95830c45ea933ad2d8ef4a8dd4ec49d8907337c
Yeondam Park <giyen9088@gmail.com>
2019-01-14 19:03:32 -0800

    Add CoinGrid

---
+++ src/Settings/CoinGrid.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import { AppContext } from "../App/AppProvider";
+
+export const CoinGridStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+`;
+
+const CoinGrid = () => (
+  <AppContext.Consumer>
+    {({ coinList }) => {
+      return (
+        <CoinGridStyled>
+          {Object.keys(coinList).map(coinKey => (
+            <div>{coinKey}</div>
+          ))}
+        </CoinGridStyled>
+      );
+    }}
+  </AppContext.Consumer>
+);
+
+export default CoinGrid;
--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButton from "./ConfirmButton";
 import Page from "../Shared/Page";
+import CoinGrid from "./CoinGrid";
 
 const Settings = () => (
   <Page name="settings">
     <WelcomeMessage />
     <ConfirmButton />
+    <CoinGrid />
   </Page>
 );
 
