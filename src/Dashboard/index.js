a23aedf67bf755f03632d65a8950b900a0ef2335
Yeondam Park <giyen9088@gmail.com>
2019-01-18 21:22:21 -0800

    Add CoinSpotlight

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,9 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
 import Page from "../Shared/Page";
 import PriceGrid from "./PriceGrid";
+import CoinSpotlight from "./CoinSpotlight";
+
+const ChartGrid = styled.div`
+  display: grid;
+  margin-top: 20px;
+  grid-gap: 15px;
+  grid-template-columns: 1fr 3fr;
+`;
+
 const Dashboard = () => (
   <Page name="dashboard">
     <PriceGrid />
+    <ChartGrid>
+      <CoinSpotlight />
+      <div>Chart goes here</div>
+    </ChartGrid>
   </Page>
 );
 
--- src/Shared/CoinImage.js
+++ src/Shared/CoinImage.js
@@ -1,10 +1,22 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
-const CoinImage = ({ coin, style }) => (
-  <img
+const CoinImageStyled = styled.img`
+  height: 50px;
+  ${props =>
+    props.spotlight &&
+    css`
+      height: 200px;
+      margin: auto;
+      display: block;
+    `}
+`;
+
+const CoinImage = ({ coin, spotlight }) => (
+  <CoinImageStyled
+    spotlight={spotlight}
     src={`http://cryptocompare.com/${coin.ImageUrl}`}
     alt={coin.CoinSymbol}
-    style={style || { height: "50px" }}
   />
 );
 
